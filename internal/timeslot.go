ab342eb16e6d6cc129d05f8bde76e74eca149119
Yazeed AlKhalaf <yazeedfady@gmail.com>
2022-10-20 22:00:40 +0300

    fix an issue in the marshalling

--- internal/timeslot.go
+++ internal/timeslot.go
@@ -3,7 +3,7 @@ package internal
 type TimeSlot struct {
 	TimeStart  TimeOfDay `json:"time_start"`
 	TimeEnd    TimeOfDay `json:"time_end"`
-	CourseName string
+	CourseName string    `json:"course_name"`
 }
 
 func (ts *TimeSlot) IsFree() bool {
